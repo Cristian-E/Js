2d9a5fe052c3ea9c0cec823761325e4ff2500e27
Cristian Rodriguez <cristianrodriguez306024298@gmail.com>
2022-06-30 21:37:14 -0300

    Primer Trbajo

---
+++ asset/js/main.js
@@ -0,0 +1,9 @@
+// Trabajo 1
+
+let entrada = prompt ("ingrese su nombre")
+while (entrada != "esc"){
+    console.log (entrada)
+    entrada = prompt ("ingrese un nombre")
+}
+
+// Fin de Trabajo 1
